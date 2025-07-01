62d69566a94cda72e8066e4615233946234d1b35
Stephen Synowsky <ssynowsky@gmail.com>
2025-07-01 12:41:25 -0400

    Lesson 5 - GET, POST, PUT, DELETE

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+  {
+    id: 1,
+    name: 'John',
+    email: 'john@email.com'
+  },
+  {
+    id: 2,
+    name: 'Kyle',
+    email: 'kyle@email.com'
+  },
+  {
+    id: 3,
+    name: 'Mark',
+    email: 'mark@email.com'
+  },
+  {
+    id: 4,
+    name: 'Luke',
+    email: 'luke@email.com'
+  },
+];
+
+module.exports = users;
